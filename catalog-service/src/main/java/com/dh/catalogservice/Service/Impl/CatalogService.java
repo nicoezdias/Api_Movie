2d14f2b2d17ac31204d58ff7b1b8e92201594919
nicoezdias <nicolasezdias@gmail.com>
2022-08-26 20:27:53 -0300

    config

--- catalog-service/src/main/java/com/dh/catalogservice/Service/Impl/CatalogService.java
+++ catalog-service/src/main/java/com/dh/catalogservice/Service/Impl/CatalogService.java
@@ -4,6 +4,8 @@ import com.dh.catalogservice.Model.CatalogDto;
 import com.dh.catalogservice.Model.MovieDto;
 import com.dh.catalogservice.Service.ICatalogService;
 import com.dh.catalogservice.Service.MovieFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,8 @@ import java.util.List;
 
 @Service
 public class CatalogService implements ICatalogService {
+
+    public static Logger LOG = LoggerFactory.getLogger(CatalogService.class);
 
     private final MovieFeignClient movieFeignClient;
 
@@ -22,7 +26,7 @@ public class CatalogService implements ICatalogService {
     @Override
     public CatalogDto getCatalogByGenre(String genere) {
         ResponseEntity<List<MovieDto>> movieResponse = movieFeignClient.findMovieByGenre(genere);
-        System.out.println(movieResponse.getHeaders().get("port"));
+        LOG.info("Puerto de la Instancia: " + movieResponse.getHeaders().get("port"));
         if (movieResponse.getStatusCode().is2xxSuccessful())
             return new CatalogDto(genere, movieResponse.getBody());
         return null;
